0a437c1be47c01fd55f6c0b8a06111b1373133af
dennis <sanjosedennis7593@gmail.com>
2023-09-03 22:36:16 +0800

    components/button: add disabled props and update style

--- src/app/_components/forms/button.tsx
+++ src/app/_components/forms/button.tsx
@@ -2,26 +2,34 @@
 
 import React from 'react';
 
-type ButtonStyle =  'primary' | 'secondary' | 'access' | 'info' | 'success' | 'warning' | 'error' | undefined;
+type ButtonStyle = 'primary' | 'secondary' | 'access' | 'info' | 'success' | 'warning' | 'error' | undefined;
 
 type Props = {
     buttonStyle?: ButtonStyle | undefined
     customClass?: string | undefined
     children: string | React.ReactNode | undefined
+    disabled?: boolean
     onClick?: React.MouseEventHandler<HTMLButtonElement>
     type?: "button" | "submit" | "reset" | undefined
 };
 
 const Button = (props: Props) => {
-    const { 
-        onClick, 
-        type, 
-        buttonStyle = 'primary', 
+    const {
+        onClick,
+        type,
+        buttonStyle = 'primary',
         customClass = '',
-        children = ''
+        children = '',
+        disabled
     } = props;
 
-    return <button type={type} onClick={onClick} className={`btn btn-${buttonStyle} ${customClass}`}>{children}</button>
+    return <button
+        type={type}
+        onClick={onClick}
+        className={`btn btn-${buttonStyle} ${customClass}`}
+        disabled={disabled}>
+        {children}
+    </button>
 };
 
 export default Button;
