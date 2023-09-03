3078fb8d86650c4c4b73240557110dc45a379f24
dennis <sanjosedennis7593@gmail.com>
2023-09-03 22:44:40 +0800

    feat/dashboard: set as protected route

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,15 +1,24 @@
+'use client'
 import Navigation from '@/app/_components/menu/navigation';
+import WithAuth from '@/app/_components/routes/protected';
+
 
 export default function RootLayout({
     children,
 }: {
     children: React.ReactNode
 }) {
+
+
+
     return (
         <main className="bg-base-200">
-            <Navigation>
-            {children}
-            </Navigation>
+            <WithAuth>
+                <Navigation>
+                    {children}
+                </Navigation>
+            </WithAuth>
+
         </main>
     )
 }
