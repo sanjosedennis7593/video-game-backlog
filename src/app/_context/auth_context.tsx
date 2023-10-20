025e0c946d69cd511cffa66f38ba3ca8ba54603d
dennis <sanjosedennis7593@gmail.com>
2023-10-20 22:40:04 +0800

    context: add types

--- src/app/_context/auth_context.tsx
+++ src/app/_context/auth_context.tsx
@@ -1,34 +1,63 @@
 'use client'
-import React, { ReactNode, createContext, useContext ,useEffect, useState }  from 'react';
+import React, { ReactNode, createContext, useContext, useEffect, useState } from 'react';
 import {
     onAuthStateChanged,
     getAuth
 } from 'firebase/auth';
 
-import firebaseInstance from '@/config/firebase';
+import { firebaseInstance } from '@/config/firebase';
+
+// TYPES 
+import { User } from '@/types/user';
 
 const auth = getAuth(firebaseInstance);
 
-export const AuthContext = createContext({ user: null });
+type AuthContextType = {
+    user: User
+};
+
+export const AuthContext = createContext<AuthContextType>({
+    user: {
+        uid: '',
+        email: '',
+        displayName: ''
+    }
+});
 
 export const useAuthContext = () => useContext(AuthContext);
 
 export const AuthContextProvider = ({
     children,
-  }: {
+}: {
     children: ReactNode
-  }) => {
-    const [user, setUser] = useState<any>(null);
+}) => {
+    const [user, setUser] = useState<User>({
+        uid: '',
+        email: '',
+        displayName: ''
+    });
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (user) => {
-        
+
             if (user) {
-   
-                setUser(user);
+
+                const uid = user.uid;
+                const email = user.email;
+                const displayName = user.displayName;
+
+                setUser({
+                    uid,
+                    email,
+                    displayName,
+                });
             } else {
-                setUser(null);
+                setUser({
+                    uid: '',
+                    email: '',
+                    displayName: ''
+                });
             }
             setLoading(false);
         });
---
+++ src/types/user.ts
@@ -0,0 +1,5 @@
+export type User = {
+    uid: string
+    email: string | null
+    displayName: string | null
+};
